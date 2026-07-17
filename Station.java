398bc1becd23077add067d1195569d48d8e9f11f
jayy10-codes <jayy10@1utar.my>
2026-07-17 20:15:56 +0800

    Add files via upload

---
+++ Station.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Station 
+{
+	private String stationId;
+	private String name;
+	private String location;
+	
+	public Station() 
+	{
+		public Station(String stationId, String name, String location) 
+		{
+			
+			public String getStationId()
+			{
+				return stationId;
+			}
+			
+			public String getName()
+			{
+				return name;
+			}
+			
+			public String getLocation()
+			{
+				return location;
+			}
+			
+			
+		}
+	}
+
+}
---
+++ Train.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Train 
+{
+	private String trainId;
+	private String trainName;
+	private int capacity;
+	
+	public Train()
+	{
+		public Train(String trainId, String trainName, int capacity) 
+		{
+			
+			
+		}
+		
+		public String getTrainId()
+		{
+			return trainId;
+		}
+		
+		public String getTrainName()
+		{
+			return trainNmae;
+		}
+		
+		public int getCapacity()
+		{
+			return capacity;
+		}
+	}
+
+}
