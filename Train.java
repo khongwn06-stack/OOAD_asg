0a0b5d2ab325a8a07ebeb6ff9a7c9b683917d115
jayy10-codes <jayy10@1utar.my>
2026-07-27 22:39:01 +0800

    Train.java

--- Train.java
+++ Train.java
@@ -6,28 +6,44 @@ public class Train
 	private String trainName;
 	private int capacity;
 	
-	public Train()
-	{
-		public Train(String trainId, String trainName, int capacity) 
+
+	public Train(String aTrainId, String aTrainName, int aCapacity) 
 		{
-			
-			
+			trainId = aTrainId;
+			trainName = aTrainName;
+			capacity = aCapacity;
 		}
 		
-		public String getTrainId()
+	public String getTrainId()
 		{
 			return trainId;
 		}
 		
-		public String getTrainName()
+	public String getTrainName()
 		{
 			return trainNmae;
 		}
 		
-		public int getCapacity()
+	public int getCapacity()
 		{
 			return capacity;
 		}
-	}
+	
+	public void setTrainName(String newTrainName)
+		{
+			trainName = newTrainName;
+		}
 
+	public void setCapacity(String newCapacity)
+		{
+			capacity = newCapacity;
+		}
+
+	public void displayInfo()
+		{
+			System.out.println("Train ID       : " + trainId);
+			System.out.println("Train Name     : " + trainName);
+			System.out.println("Train Capacity : " + capacity);
+		}
 }
+	
