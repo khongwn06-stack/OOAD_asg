aa129ea696216c7834cd67ede073402e45d5192c
jayy10-codes <jayy10@1utar.my>
2026-07-27 22:39:51 +0800

    main.java

--- main.java
+++ main.java
@@ -8,10 +8,7 @@ public class main
 		System.out.println("  SMART METRO TICKETING SYSTEM  ");
 		System.out.println("================================\n");
 		
-		Station station1 = new Station("ST001", "KL Sentral", "Kuala Lumpur");
-		Station station2 = new Station("ST002", "Central Market", "Kuala Lumpur");
-		
-		Train train1 = new Train("TR001", "KTM", 300);
+	
 		
 		
 	}
