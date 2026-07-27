f9c8925b8db1f897a31618e6cbc3b101dcec77ed
jayy10-codes <jayy10@1utar.my>
2026-07-27 22:28:59 +0800

    Station.java

--- Station.java
+++ Station.java
@@ -3,31 +3,50 @@ package model;
 public class Station 
 {
 	private String stationId;
-	private String name;
+	private String stationName;
 	private String location;
-	
-	public Station() 
+
+public Station(String aStationId, String aStationName, String aLocation)
 	{
-		public Station(String stationId, String name, String location) 
-		{
-			
-			public String getStationId()
-			{
-				return stationId;
-			}
-			
-			public String getName()
-			{
-				return name;
-			}
-			
-			public String getLocation()
-			{
-				return location;
-			}
-			
-			
-		}
+		stationId = aStationId;
+		stationName = aStationName;
+		location = aLocaltion;
 	}
 
+public String getStationId()
+	{
+		return stationId;
+	}
+
+public String getStationName()
+	{ 
+		return stationName;
+	}
+
+public String getLocation()
+	{
+		return location;
+	}
+
+public void setStationName(String newStationName)
+	{
+		stationName = newStationName;
+	}
+
+public void setLocation(String newLocation)
+	{
+		location = newLocation;
+	}
+
+public void displayInfo()
+	{
+		System.out.println("Station ID   : " + stationId);
+		System.out.println("Station Name : " + stationName);
+		System.out.println("Location     : " + location);
+	}
 }
+
+	
+		
+	
+	
