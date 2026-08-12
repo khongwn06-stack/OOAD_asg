0ffe0c9675de752610e73878c5ebf80f2e190556
jean-chickychick <jeanlau9@gmail.com>
2026-08-12 16:44:17 +0800

    Update Route.java

--- Route.java
+++ Route.java
@@ -1,52 +1,62 @@
 package model;
 
-public class Route {
+public class Route 
+{
 
     private String routeId;
     private Station source;
     private Station destination;
     private double distanceKm;
 
-    public Route(String routeId, Station source, Station destination, double distanceKm) {
+    public Route(String routeId, Station source, Station destination, double distanceKm) 
+    {
         this.routeId = routeId;
         this.source = source;
         this.destination = destination;
         this.distanceKm = distanceKm;
     }
 
-    public String getRouteId() {
+    public String getRouteId() 
+    {
         return routeId;
     }
 
-    public Station getSource() {
+    public Station getSource() 
+    {
         return source;
     }
 
-    public Station getDestination() {
+    public Station getDestination() 
+    {
         return destination;
     }
 
-    public double getDistanceKm() {
+    public double getDistanceKm() 
+    {
         return distanceKm;
     }
 
-    public double calculateDistance() {
+    public double calculateDistance() 
+    {
         return distanceKm;
     }
 
-    public void displayRoute() {
+    public void displayRoute() 
+    {
         System.out.println("Route ID     : " + routeId);
         System.out.println("From         : " + source.getStationName());
         System.out.println("To           : " + destination.getStationName());
         System.out.printf ("Distance     : %.2f km%n", distanceKm);
     }
 
-    public String toFileString() {
+    public String toFileString() 
+    {
         return routeId + "," + source.getStationId() + "," + destination.getStationId() + "," + distanceKm;
     }
 
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return "Route ID: " + routeId
                 + " | From: " + source.getStationName()
                 + " -> To: " + destination.getStationName()
