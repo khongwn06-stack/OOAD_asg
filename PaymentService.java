3c38d277a86104a4833f47e817361fe1d9a65dc6
jxuen3654 <jxuen331@1utar.my>
2026-08-13 20:33:14 +0800

    Update PaymentService.java

--- PaymentService.java
+++ PaymentService.java
@@ -1,23 +1,71 @@
 package service;
 
 import payment.Payment;
+import payment.PaymentRecord;
+import payment.CashPayment;
+import payment.CardPayment;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
 
 public class PaymentService
 {
+    private int nextPaymentId = 1001;
+
     public boolean processPayment(Payment payment, double amount)
-  {
+    {
         if (payment == null)
         {
-            System.out.println("Payment method cannot be null.");
+            System.out.println("Please select a payment me.");
             return false;
         }
 
         if (amount <= 0)
         {
-            System.out.println("Payment amount must be greater than RM 0.");
+            System.out.println("Payment failed: invalid amount.");
             return false;
         }
 
-        return payment.pay(amount);
+        boolean success = payment.pay(amount);
+
+        if (success)
+        {
+            String paymentMethod;
+
+            if (payment instanceof CashPayment)
+            {
+                paymentMethod = "Cash";
+            }
+            else if (payment instanceof CardPayment)
+            {
+                paymentMethod = "Card";
+            }
+
+            String paymentId = String.format("P%04d", nextPaymentId++);
+
+            PaymentRecord record = new PaymentRecord(paymentId, paymentMethod, amount);
+
+            savePayment(record);
+        }
+
+        return success;
+    }
+
+    private void savePayment(PaymentRecord record)
+    {
+        try
+        {
+            FileWriter fw = new FileWriter("payments.txt", true);
+            PrintWriter outputFile = new PrintWriter(fw);
+
+            outputFile.println(record.toFileLine());
+
+            outputFile.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error writing to file.");
+        }
     }
 }
