4801d883c23668aa5b914c7551a4f22658979bcd
jxuen3654 <jxuen331@1utar.my>
2026-08-13 14:54:32 +0800

    Update CashPayment.java

--- CashPayment.java
+++ CashPayment.java
@@ -1,1 +1,20 @@
+package payment;
 
+public class CashPayment implements Payment
+{
+    public boolean pay(double amount) 
+    {
+        if (amount <= 0)
+        {
+            System.out.println("Cash payment failed: invalid amount.");
+            return false;
+        }
+
+        else
+        {
+          System.out.printf("Cash payment successful: RM %.2f%n received", amount);
+          return true;
+        }
+        
+    }
+}
