b4c58428b0cc29139beca71da4d3f4fbdaa86826
jxuen3654 <jxuen331@1utar.my>
2026-08-13 15:30:38 +0800

    Update CardPayment.java

--- CardPayment.java
+++ CardPayment.java
@@ -13,3 +13,28 @@ public class CardPayment implements Payment
     {
         this.cardNumber = cardNumber;
     }
+
+    @Override
+    public boolean pay(double amount)
+    {
+
+        if (amount <= 0)
+        {
+            System.out.println("Card payment failed: invalid amount.");
+            return false;
+        }
+
+        else
+        {
+            System.out.printf("Card payment successful: RM %.2f received%n", amount);
+            return true;
+        }
+        
+        //System.out.printf("Card payment successful using card ending in %s: RM %.2f%n", getMaskedCardNumber(), amount);
+    }
+
+
+
+
+
+
