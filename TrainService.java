86ef7f740463c99ea6065f0fb4be3465c733cb53
jayy10-codes <jayy10@1utar.my>
2026-08-15 01:43:13 +0800

    TrainService.java

--- TrainService.java
+++ TrainService.java
@@ -5,6 +5,7 @@ import Model.Train;
 
 public class TrainService
 {
+   
     private ArrayList<Train> trains;
 
     public TrainService()
@@ -12,27 +13,29 @@ public class TrainService
         trains = new ArrayList<Train>();
     }
 
+    
     public void addTrain(Train train)
     {
         trains.add(train);
     }
 
+   
     public void viewTrains()
     {
-        System.out.println("---------------------------------");
+        System.out.println("----------------------------------");
         System.out.println("Train List");
-        System.out.println("---------------------------------");
+        System.out.println("----------------------------------");
 
         for(int i = 0; i < trains.size(); i++)
         {
             trains.get(i).displayTrain();
-            System.out.println("---------------------------------");
+            System.out.println("----------------------------------");
         }
     }
 
+    
     public ArrayList<Train> getTrains()
     {
         return trains;
     }
-
 }
