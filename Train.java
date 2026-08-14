a02f0d6109c3bad65848a62e73042ff46b3f06b6
jayy10-codes <jayy10@1utar.my>
2026-08-15 01:29:35 +0800

    Train.java

--- Train.java
+++ Train.java
@@ -1,49 +1,37 @@
-package model;
+package Model;
 
-public class Train 
+public class Train
 {
-	private String trainId;
-	private String trainName;
-	private int capacity;
-	
+    private String trainId;
+    private String trainName;
+    private int capacity;
 
-	public Train(String aTrainId, String aTrainName, int aCapacity) 
-		{
-			trainId = aTrainId;
-			trainName = aTrainName;
-			capacity = aCapacity;
-		}
-		
-	public String getTrainId()
-		{
-			return trainId;
-		}
-		
-	public String getTrainName()
-		{
-			return trainNmae;
-		}
-		
-	public int getCapacity()
-		{
-			return capacity;
-		}
-	
-	public void setTrainName(String newTrainName)
-		{
-			trainName = newTrainName;
-		}
+    public Train(String aTrainId, String aTrainName, int aCapacity)
+    {
+        trainId = aTrainId;
+        trainName = aTrainName;
+        capacity = aCapacity;
+    }
 
-	public void setCapacity(String newCapacity)
-		{
-			capacity = newCapacity;
-		}
+    public String getTrainId()
+    {
+        return trainId;
+    }
 
-	public void displayInfo()
-		{
-			System.out.println("Train ID       : " + trainId);
-			System.out.println("Train Name     : " + trainName);
-			System.out.println("Train Capacity : " + capacity);
-		}
+    public String getTrainName()
+    {
+        return trainName;
+    }
+
+    public int getCapacity()
+    {
+        return capacity;
+    }
+
+    public void displayTrain()
+    {
+        System.out.println("Train ID   : " + trainId);
+        System.out.println("Train Name : " + trainName);
+        System.out.println("Capacity   : " + capacity);
+    }
 }
-	
