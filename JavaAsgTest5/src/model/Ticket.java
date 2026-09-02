5f9c12b71361dc1644e8ee15d593a6b6229f9487
jsng-cmyk <jsng8959@1utar.my>
2026-09-02 17:21:13 +0800

    Refactor Ticket class and improve printTicket output
    Updated ticket details formatting and added header/footer in printTicket method.

--- JavaAsgTest5/src/model/Ticket.java
+++ JavaAsgTest5/src/model/Ticket.java
@@ -87,7 +87,7 @@ public class Ticket
     
     public void printTicket() 
     {
-        System.out.println();
+        System.out.println("\n=============TICKET DETAILS==============");
         System.out.println("Ticket ID   : " + ticketId);
         System.out.println("Passenger   : " + passenger.userId);
         System.out.println("From        : " + source.getStationName());
@@ -95,7 +95,7 @@ public class Ticket
         System.out.println("Ticket Type : " + ticketType);
         System.out.printf ("Fare        : RM %.2f%n", fare);
         System.out.println("Status      : " + status);
-        System.out.println();
+        System.out.println("=========================================\n");
     }
 
 
